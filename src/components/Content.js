339279a1197a8ac00edeb287b06eda86ce1299f5
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2024-04-12 15:52:50 +0000

    content done

--- src/components/Content.js
+++ src/components/Content.js
@@ -18,10 +18,9 @@ import { SlOptionsVertical } from "react-icons/sl"; // Options icon
 export default function Content() {
   return (
     // Root div
-    <div>
+    <div className=" text-xs">
       {/* Header of main content */}
       <div className="flex">
-
         <div className="flex basis-4/5 mt-3 items-center">
           <div className=" ml-3 mr-3">
             <MdOutlineSpaceDashboard />
@@ -54,36 +53,40 @@ export default function Content() {
           </div>
         </div>
       </div>
+
       {/* Main content */}
 
-      <div className="flex">
-        <div className=" basis-13/15">
-          <div>Getting Started</div>
-          <div className="flex items-start">
+      <div className="flex text-sm">
+        <div className="basis-13/15 px-40">
+          <div className=" text-3xl font-bold my-4"> Getting started </div>
+          <div className="flex items-start  bg-gray-300 p-3 mb-6">
             <div className="mt-1">
-
-            <FaLock />
+              <FaLock />
             </div>
-            <div>
-              This doc in your private channel, where you can store your private
+            <div className=" ml-4">
+              This doc in your <b>private channel</b>, where you can store your private
               work. You can share docs one by one from here, but to properly
-              organise your team knowledge base, use workspace channels. <br />{" "}
+              organise your team knowledge base, use <b>workspace channels</b>. <br />{" "}
               <br />
               You can make channels <b>public</b> to make all docs visible to
               everyone on the team, or <b>private</b> if you want the channel's
               docs to only be visible by a selection of teammates.
             </div>
           </div>
-          <div>Collaborate with your team with these three features.</div>
+          <div className=" text-xl font-bold">
+            Collaborate with your team with these three <br /> features.
+          </div> <br />
           <div className="flex items-center">
-            <FaUserPen />
+            <div>
+              <FaUserPen />
+            </div>
 
-            <div>Docs</div>
+            <div className=" text-lg pr-8">Docs</div>
           </div>
-          <div>
+          <div className=" mb-3">
             <img src={docs} alt="docs" />
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center bg-gray-200 p-1">
             <IoBulbOutline />
 
             <div>Give it a go by typing / below</div>
@@ -91,7 +94,7 @@ export default function Content() {
         </div>
 
         {/* Theme and FAQ  */}
-        <div className=" basis-2/15 mr-3">
+        <div className=" basis-2/15 mr-5 items-end content-end mb-5">
           <div>
             <IoMoonOutline />
           </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,24 @@ import React from "react";
 // Bell Icon
 import { FaRegBell } from "react-icons/fa";
 // Close Icon
-import { IoIosClose } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+
 
 export default function Header() {
   return (
-    <div className="flex bg-blue-400 text-white p-3 justify-end align-middle">
-      <div className="">
-        Enable browser Notifications to avoid missing out on important activity
+    <div className="flex bg-blue-500 text-white justify-end items-center p-1 text-sm">
+      <div className=" basis-9/12 text-center">
+        Enable browser Notifications to avoid missing out on important activity.
       </div>
       {/* Div 2 */}
-      <div className="flex bg-blue-800 p-1">
-        <div> 
+      <div className="flex bg-blue-700 p-1 basis-2/12 items-center justify-center rounded-md ">
+        <div > 
           <FaRegBell />
         </div>
-        <div>Enable Notifications</div>
+        <div className= "ml-2" >Enable Notifications</div>
       </div>
-      <div className=" mt-2">
-        <IoIosClose/>
+      <div className=" basis-1/12">
+        <IoClose className=" ml-11"/>
       </div>
     </div>
   );
